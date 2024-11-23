48edad5659a521476d36f856849a407dffc945ae
Deepanshu Jain <deepanshujain1234@live.com>
2024-11-23 12:17:53 +0700

    Add wezterm and tmux config files

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+local config = {}
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
+config.color_scheme = "Batman"
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 14.0
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
+config.native_macos_fullscreen_mode = true
+return config
