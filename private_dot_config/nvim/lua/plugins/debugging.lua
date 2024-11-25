31f2a1fc315d42c13061f6a1716644063cbfe6b3
Deepanshu Jain <deepanshujain1234@live.com>
2024-11-25 22:32:43 +0700

    Update .config/nvim/lua/plugins/debugging.lua

--- private_dot_config/nvim/lua/plugins/debugging.lua
+++ private_dot_config/nvim/lua/plugins/debugging.lua
@@ -5,10 +5,33 @@ return {
       {
         "rcarriga/nvim-dap-ui",
         dependencies = {
-					"nvim-neotest/nvim-nio",
-				},
-        keys = {
-          {"n", "<leader>du", "<cmd>lua require('dapui').toggle()<CR>", "Toggle Debug UI"},
+          "nvim-neotest/nvim-nio",
+        },
+        config = function(_, opts)
+          local dap, dapui = require("dap"), require("dapui")
+          dapui.setup(opts)
+          dap.listeners.after.event_initialized["dapui_config"] = function()
+            dapui.open({})
+          end
+          setKeymap("n", "<leader>du", dapui.toggle({}), "Toggle Debug UI")
+          setKeymap({ "n", "v" }, "<leader>de", dapui.eval(), "Eval")
+        end,
+      },
+      {
+        "theHamsta/nvim-dap-virtual-text",
+        opts = {},
+      },
+      {
+        "jay-babu/mason-nvim-dap.nvim",
+        dependencies = {
+          "mfussenegger/nvim-dap",
+          "williamboman/mason.nvim",
+        },
+        cmd = { "DapInstall", "DapUninstall" },
+        opts = {
+          automatic_installation = true,
+          handlers = {},
+          ensure_installed = {},
         },
       },
     },
@@ -28,27 +51,26 @@ return {
         dapui.close()
       end
 
-      dap.configurations.scala = {
-        {
-          type = "scala",
-          request = "launch",
-          name = "RunOrTest",
-          metals = {
-            runType = "runOrTestFile",
-            --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
-          },
-        },
-        {
-          type = "scala",
-          request = "launch",
-          name = "Test Target",
-          metals = {
-            runType = "testTarget",
-          },
-        },
-      }
+      -- dap.configurations.scala = {
+      --   {
+      --     type = "scala",
+      --     request = "launch",
+      --     name = "RunOrTest",
+      --     metals = {
+      --       runType = "runOrTestFile",
+      --       --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+      --     },
+      --   },
+      --   {
+      --     type = "scala",
+      --     request = "launch",
+      --     name = "Test Target",
+      --     metals = {
+      --       runType = "testTarget",
+      --     },
+      --   },
+      -- }
 
-      -- setKeymap("n", "<leader>du", dapui.toggle, "Toggle Debug UI")
       setKeymap("n", "<leader>dt", dap.toggle_breakpoint, "Toggle Breakpoint")
       setKeymap("n", "<leader>dc", dap.continue, "Continue Debugging")
     end,
