8426648ee75e07e50ad6d15988dc195cb8cb0ea9
Deepanshu Jain <deepanshujain1234@live.com>
2024-12-07 16:10:02 +0700

    add wezterm

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require("wezterm")
+local config = {}
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
+config.color_scheme = "Catppuccin Macchiato"
+
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 14.0
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
+config.native_macos_fullscreen_mode = true
+config.automatically_reload_config = true
+
+return config
