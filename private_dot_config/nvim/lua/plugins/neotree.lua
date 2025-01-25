b90e8432271a6c1b866f604308df9701f23e4586
Deepanshu Jain <deepanshujain1234@live.com>
2025-01-25 14:55:03 +0700

    Change show to focus in neotree

--- private_dot_config/nvim/lua/plugins/neotree.lua
+++ private_dot_config/nvim/lua/plugins/neotree.lua
@@ -8,9 +8,9 @@ return {
             "MunifTanjim/nui.nvim",
         },
         config = function()
-            setKeymap("n", "<leader>ef", "<CMD> Neotree toggle show filesystem left <CR>", "Toggle Neotree")
-            setKeymap("n", "<leader>eb", "<CMD> Neotree toggle show buffers left <CR>", "Toggle Neotree")
-            setKeymap("n", "<leader>eg", "<CMD> Neotree toggle show git_status left <CR>", "Toggle Neotree")
+            setKeymap("n", "<leader>ef", "<CMD> Neotree toggle focus filesystem left <CR>", "Toggle Neotree")
+            setKeymap("n", "<leader>eb", "<CMD> Neotree toggle focus buffers left <CR>", "Toggle Neotree")
+            setKeymap("n", "<leader>eg", "<CMD> Neotree toggle focus git_status left <CR>", "Toggle Neotree")
         end
     }
 }
