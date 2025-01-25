2edd96fba7dd48486813864f364b8c38f884558e
Deepanshu Jain <deepanshujain1234@live.com>
2025-01-25 15:24:20 +0700

    Fix neotree

--- private_dot_config/nvim/lua/plugins/neotree.lua
+++ private_dot_config/nvim/lua/plugins/neotree.lua
@@ -8,6 +8,16 @@ return {
             "MunifTanjim/nui.nvim",
         },
         config = function()
+            require("neo-tree").setup({
+                close_if_last_window = true,
+                buffers = {
+                    follow_current_file = { enabled = true },
+                },
+
+                filesystem = {
+                    follow_current_file = { enabled = true },
+                },
+            })
             setKeymap("n", "<leader>ef", "<CMD> Neotree toggle focus filesystem left <CR>", "Toggle Neotree")
             setKeymap("n", "<leader>eb", "<CMD> Neotree toggle focus buffers left <CR>", "Toggle Neotree")
             setKeymap("n", "<leader>eg", "<CMD> Neotree toggle focus git_status left <CR>", "Toggle Neotree")
