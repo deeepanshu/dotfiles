ba20185e82f03815914a73cbf95717af70c155d9
Deepanshu Jain <deepanshujain1234@live.com>
2025-08-17 13:28:59 +0700

    some updates

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -3,8 +3,8 @@ local config = {}
 local mux = wezterm.mux
 
 wezterm.on("gui-startup", function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
 end)
 
 config.color_scheme = "Catppuccin Macchiato"
@@ -12,8 +12,17 @@ config.color_scheme = "Catppuccin Macchiato"
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 16.0
 config.window_decorations = "RESIZE"
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 config.native_macos_fullscreen_mode = true
 config.automatically_reload_config = true
+config.tab_max_width = 16
+
+config.keys = {
+	{
+		key = "Enter",
+		mods = "SHIFT",
+		action = wezterm.action.SendString("\n"),
+	},
+}
 
 return config
